0b1eefb77a5f89bb5028210007838f7b510772f6
unknown <dineshthiru7star@gmail.com>
2023-04-09 06:00:38 -0700

    First commit

---
+++ src/environments/firebaseConfig.ts
@@ -0,0 +1,24 @@
+export const FireBaseConfig = {
+  /* apiKey: "AIzaSyCd0wii8iVgaR8M5zpHraH7VrrHncjIjDE",
+    authDomain: "blog-app-3cda4.firebaseapp.com",
+    databaseURL: "https://blog-app-3cda4.firebaseio.com",
+    projectId: "blog-app-3cda4",
+    storageBucket: "blog-app-3cda4.appspot.com",
+    messagingSenderId: "1045419816065", 
+
+       apiKey: "AIzaSyCd0wii8iVgaR8M5zpHraH7VrrHncjIjDE",
+    authDomain: "blog-app-3cda4.firebaseapp.com",
+    databaseURL: "https://blog-app-3cda4.firebaseio.com",
+    projectId: "blog-app-3cda4",
+    storageBucket: "blog-app-3cda4.appspot.com",
+    messagingSenderId: "1045419816065",
+    appId: "1:1045419816065:web:6de6e8117841a8322bc980",*/
+    apiKey: 'AIzaSyBd4LGn0wBpUW4QxXWaW-m-RvL54zHr42Q',
+    authDomain: 'localhost:4200',
+    databaseURL: 'https://blogging-app-da314-default-rtdb.firebaseio.com',
+    projectId: 'blogging-app-da314',
+    storageBucket: 'blogging-app-da314.appspot.com',
+    messagingSenderId: '651600420229',
+    appId: "1:1045419816065:web:6de6e8117841a8322bc980",
+    
+};
